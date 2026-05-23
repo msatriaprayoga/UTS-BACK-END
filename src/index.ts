a2f7d37c9c9cc911ea70d3d08e4bb80384a30e52
msatriaprayoga <msatriaprayoga92@gmail.com>
2026-05-23 14:11:04 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import routes from './routes/index';
-
-dotenv.config();
-
 const express = require("express");
 const cors = require("cors");
 
