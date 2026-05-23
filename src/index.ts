32e93ebb9f06504a4d98142f7760265c0ab0735b
msatriaprayoga <msatriaprayoga92@gmail.com>
2026-05-23 09:08:14 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import routes from './routes/index';
-
-dotenv.config();
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (
-      !origin ||
-      origin.includes("vercel.app") ||
-      origin === "http://localhost:5173"
-    ) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "https://uts-front-end-six.vercel.app"
+  ],
+  credentials: true
 }));
-app.options('*', cors()); 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.get('/health', (_req, res) => {
-  res.json({ status: 'ok', message: 'Event Management API is running' });
+app.post("/api/auth/login", (req, res) => {
+  res.json({
+    message: "Login berhasil"
+  });
 });
 
-app.use('/api', routes);
-
-app.listen(PORT, () => {
-  console.log(`Server Berjalan di port ${PORT}`);
-});
-
-export default app;
+module.exports = app;
